e3847e05cb81e5d47cbe8950be7eb8174e007480
Rick Martens <info@rickmartens.nl>
2021-10-04 10:40:57 +0200

    Fix

--- src/main/java/com/example/api/v1/users/V1GetUserController.java
+++ src/main/java/com/example/api/v1/users/V1GetUserController.java
@@ -49,9 +49,9 @@ public class V1GetUserController implements Controller {
             String[] path = httpRequest.getRelativePath().toLowerCase().split("/");
             String controllerName;
             if (isVersioned(httpRequest)) {
-                controllerName = path.length > 4 ? path[3] : null;
+                controllerName = path.length >= 4 ? path[3] : null;
             } else {
-                controllerName = path.length > 3 ? path[2] : null;
+                controllerName = path.length >= 3 ? path[2] : null;
             }
 
             Controller controller = controllers.get(controllerName);
