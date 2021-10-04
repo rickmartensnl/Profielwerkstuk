7afa89c58dba232c0365b898a428ca91ec2ae48f
Rick Martens <info@rickmartens.nl>
2021-10-04 10:26:09 +0200

    Session Controller

--- src/main/java/com/example/api/v1/users/V1GetUserController.java
+++ src/main/java/com/example/api/v1/users/V1GetUserController.java
@@ -1,5 +1,6 @@
 package com.example.api.v1.users;
 
+import com.example.api.v1.users.sessions.V1SessionController;
 import com.example.database.impl.UserManager;
 import com.example.exceptions.DatabaseOfflineException;
 import com.example.middlewares.AuthMiddleware;
@@ -8,13 +9,18 @@ import io.activej.http.HttpRequest;
 import io.activej.http.HttpResponse;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class V1GetUserController implements Controller {
 
+    public final Map<String, Controller> controllers = new HashMap<>();
     private final UUID uuid;
 
     public V1GetUserController(String uuid, HttpRequest httpRequest) {
+        controllers.put("sessions", new V1SessionController());
+
         System.out.println(uuid);
         if (uuid.equalsIgnoreCase("@me")) {
             this.uuid = AuthMiddleware.getSubject(httpRequest);
@@ -40,17 +46,37 @@ public class V1GetUserController implements Controller {
                 return HttpResponse.notFound404().withJson("{\"message\":\"404: Not Found\",\"code\":\"user not found\"}");
             }
 
-            UUID authUUID = AuthMiddleware.getSubject(httpRequest);
+            String[] path = httpRequest.getRelativePath().toLowerCase().split("/");
+            String controllerName;
+            if (isVersioned(httpRequest)) {
+                controllerName = path[3];
+            } else {
+                controllerName = path[2];
+            }
 
-            if (authUUID == null) {
+            Controller controller = controllers.get(controllerName);
+
+            if (controller != null) {
+                if (!isAllowedMethod(httpRequest, controller)) {
+                    return methodNotAllowed405();
+                }
+
+                callMiddleware(httpRequest, controller);
+
+                return controller.runRequest(httpRequest);
+            } else {
+                UUID authUUID = AuthMiddleware.getSubject(httpRequest);
+
+                if (authUUID == null) {
+                    return HttpResponse.ok200().withJson(user.getJsonObject());
+                }
+
+                if (user.getUuid().equals(authUUID)) {
+                    return HttpResponse.ok200().withJson(user.getJsonObject(true));
+                }
+
                 return HttpResponse.ok200().withJson(user.getJsonObject());
             }
-
-            if (user.getUuid().equals(authUUID)) {
-                return HttpResponse.ok200().withJson(user.getJsonObject(true));
-            }
-
-            return HttpResponse.ok200().withJson(user.getJsonObject());
         } catch (DatabaseOfflineException exception) {
             return HttpResponse.ofCode(500).withJson("{\"message\":\"" + exception.getClass().getName() + "\"}");
         }
---
+++ src/main/java/com/example/api/v1/users/sessions/V1SessionController.java
@@ -0,0 +1,21 @@
+package com.example.api.v1.users.sessions;
+
+import com.example.middlewares.AuthMiddleware;
+import com.example.utils.AllowMethods;
+import com.example.utils.Controller;
+import com.example.utils.UseMiddleware;
+import io.activej.http.HttpMethod;
+import io.activej.http.HttpRequest;
+import io.activej.http.HttpResponse;
+import org.jetbrains.annotations.NotNull;
+
+@UseMiddleware({ AuthMiddleware.class })
+@AllowMethods({ HttpMethod.GET, HttpMethod.POST, HttpMethod.PATCH })
+public class V1SessionController implements Controller {
+
+    @Override
+    public @NotNull HttpResponse runRequest(HttpRequest httpRequest) {
+        return HttpResponse.ok200().withPlainText(httpRequest.getRelativePath());
+    }
+
+}
