c0e04686061c563a4e6d479bdbac8a8ae08ea5b2
Rick Martens <info@rickmartens.nl>
2021-11-26 09:16:16 +0100

    Update Header.js

--- frontend/src/site/shared/Header.js
+++ frontend/src/site/shared/Header.js
@@ -2,14 +2,10 @@
 import React, { Fragment } from 'react'
 import { Popover, Transition, Menu } from '@headlessui/react'
 import {
-    ChartBarIcon,
-    CursorClickIcon,
+    ShieldExclamationIcon,
     MenuIcon,
-    PhoneIcon,
+    CalculatorIcon,
     PlayIcon,
-    RefreshIcon,
-    ShieldCheckIcon,
-    ViewGridIcon,
     XIcon,
     LogoutIcon,
     AcademicCapIcon,
@@ -17,38 +13,6 @@ import {
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { Link } from "react-router-dom";
-
-const solutions = [
-    {
-        name: 'Analytics',
-        description: 'Get a better understanding of where your traffic is coming from.',
-        href: '/#analytics',
-        icon: ChartBarIcon,
-    },
-    {
-        name: 'Engagement',
-        description: 'Speak directly to your customers in a more meaningful way.',
-        href: '/#engagement',
-        icon: CursorClickIcon,
-    },
-    { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-    {
-        name: 'Integrations',
-        description: "Connect with third-party tools that you're already using.",
-        href: '/#integrations',
-        icon: ViewGridIcon,
-    },
-    {
-        name: 'Automations',
-        description: 'Build strategic funnels that will drive your customers to convert',
-        href: '/#automations',
-        icon: RefreshIcon,
-    },
-]
-const callsToAction = [
-    { name: 'Watch Demo', href: '/#demo', icon: PlayIcon },
-    { name: 'Contact Sales', href: '/#sales', icon: PhoneIcon },
-]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -64,6 +28,24 @@ export class Header extends React.Component {
         };
 
         this.authMiddleware = this.props.data.authMiddleware;
+    }
+
+    getCallToActions() {
+        return [
+            { name: 'Begin met leren', href: '/app', icon: PlayIcon },
+            { name: 'Bug rapporteren', href: '/#reportbug', icon: ShieldExclamationIcon },
+        ];
+    }
+
+    getSolutions() {
+        return [
+            {
+                name: 'Wiskunde B',
+                description: 'Start gelijk met leren van Wiskunde B!',
+                href: '/#wib',
+                icon: CalculatorIcon,
+            }
+        ];
     }
 
     componentDidUpdate(prevProps) {
@@ -124,7 +106,7 @@ export class Header extends React.Component {
                                             <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
                                                 <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                                                     <div className="relative grid gap-6 bg-white dark:bg-dark-primary px-5 py-6 sm:gap-8 sm:p-8">
-                                                        {solutions.map((item) => (
+                                                        {this.getSolutions().map((item) => (
                                                             <Link key={item.name} to={item.href} className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 dark:hover:bg-dark-secondary">
                                                                 <item.icon className="flex-shrink-0 h-6 w-6 text-blue-500" aria-hidden="true" />
                                                                 <div className="ml-4">
@@ -135,7 +117,7 @@ export class Header extends React.Component {
                                                         ))}
                                                     </div>
                                                     <div className="px-5 py-5 bg-gray-50 dark:bg-dark-primary-2 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                                                        {callsToAction.map((item) => (
+                                                        {this.getCallToActions().map((item) => (
                                                             <div key={item.name} className="flow-root">
                                                                 <Link to={item.href} className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 dark:text-dark-text-secondary hover:bg-gray-100 dark:hover:bg-dark-secondary">
                                                                     <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
@@ -246,7 +228,7 @@ export class Header extends React.Component {
                                 </div>
                                 <div className="mt-6">
                                     <nav className="grid gap-y-8">
-                                        {solutions.map((item) => (
+                                        {this.getSolutions().map((item) => (
                                             <Link key={item.name} to={item.href} className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 dark:hover:bg-dark-secondary">
                                                 <item.icon className="flex-shrink-0 h-6 w-6 text-blue-500" aria-hidden="true" />
                                                 <span className="ml-3 text-base font-medium text-gray-900 dark:text-dark-text-secondary">{item.name}</span>
