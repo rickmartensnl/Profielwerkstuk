f65eaf90c6dfc87207f15e8c9521eedadc4ecc4c
rickmartensnl <info@rickmartens.nl>
2022-01-06 11:10:07 +0100

    Fix space at register.

--- frontend/src/site/auth/Register.js
+++ frontend/src/site/auth/Register.js
@@ -183,7 +183,7 @@ export class Register extends React.Component {
                                     <Link to="#privacy" className="text-blue-600 hover:underline">
                                         Privacybeleid
                                     </Link>
-                                    gelezen en ga ermee akkoord.
+                                    &nbsp;gelezen en ga ermee akkoord.
                                 </span>
                             </label>
                             <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
